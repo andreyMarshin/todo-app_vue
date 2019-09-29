5374d2f9e307542fa2dc59a943365f767da02987
Andrey Marshin <andreymarshin77@gmail.com>
2019-09-29 13:57:13 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container L3 mt-4">
+    <h1>Todo App</h1>
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+
+.container {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 40%
+}
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <input v-model="newTodo" type="text" class="form-control" placeholder="Введите название дела и нажмите enter"
+    @keyup.enter="addTodo">
+    <button class="btn btn-dark" @click="addTodo">Add</button> 
+
+    <ul class="list-group">
+      <li class="list-group-item mt4"
+       v-for="(todo, index) in todos" :key="index" @click="deleteTodo(index)">
+        {{ index + 1 }} {{ todo.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newId: 3,
+      newTodo: '',
+      todos: [
+        { 
+          id: 1,
+          title: 'Walk the dog',
+          edit: false,
+        },
+        { 
+          id: 2,
+          title: 'Read a book',
+          edit: false,
+        },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(
+        {
+          id: this.newId,
+          title: this.newTodo,
+          edit: false,
+        }
+      );
+      this.newTodo = '';
+      this.newId += 1
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1)
+    }
+  },
+}
+</script>
+
+<style>
+
+body {
+  background-color: #ccc
+}
+</style>
