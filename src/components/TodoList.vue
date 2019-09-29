87912572129aaab8530f10edf41f85ed4869ae38
Andrey Marshin <andreymarshin77@gmail.com>
2019-09-29 15:02:56 +0300

    edditing

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,8 +6,17 @@
 
     <ul class="list-group">
       <li class="list-group-item mt4"
-       v-for="(todo, index) in todos" :key="index" @click="deleteTodo(index)">
+       v-for="(todo, index) in todos" :key="index"
+      > 
         {{ index + 1 }} {{ todo.title }}
+        <button class="btn cross" @click="deleteTodo(index)">
+          &#10006;
+        </button>
+        <button class="btn" @click="todo.edit = !todo.edit">
+          &#9998;
+        </button>
+        <input class="form-control" type="text" v-if="todo.edit" 
+        v-model="todo.title">
       </li>
     </ul>
   </div>
@@ -22,12 +31,12 @@ export default {
       todos: [
         { 
           id: 1,
-          title: 'Walk the dog',
+          title: 'Reading the books',
           edit: false,
         },
         { 
           id: 2,
-          title: 'Read a book',
+          title: 'Working from home',
           edit: false,
         },
       ]
@@ -46,7 +55,11 @@ export default {
       this.newId += 1
     },
     deleteTodo(index) {
+      console.log(index)
       this.todos.splice(index, 1)
+    },
+    editingTodo() {
+      console.log()
     }
   },
 }
@@ -56,5 +69,9 @@ export default {
 
 body {
   background-color: #ccc
+}
+
+.cross {
+  float: right;
 }
 </style>
